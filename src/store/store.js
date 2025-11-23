44dbd01428d089e8a4b572a5745e7747430a689e
Layson Ndenza <laysonndenza@gmail.com>
2025-11-24 01:28:11 +0300

    new updates

--- src/store/store.js
+++ src/store/store.js
@@ -5,6 +5,7 @@ import courseReducer from "./slices/courseSlice";
 import progressReducer from "./slices/progressSlice";
 import adminReducer from "./slices/adminSlice";
 import instructorReducer from "./slices/instructorSlice";
+import reviewReducer from "./slices/reviewSlice";
 import uiReducer from "./slices/uiSlice";
 
 export const store = configureStore({
@@ -15,8 +16,15 @@ export const store = configureStore({
     progress: progressReducer,
     admin: adminReducer,
     instructor: instructorReducer,
+    reviews: reviewReducer,
     ui: uiReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
 });
 
 export default store;
